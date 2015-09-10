8d98bb271f06916e4de80fea8fdfdac2b76d51fb
Draco <draco.mystack@gmail.com>
2015-09-10 17:52:54 +0800

    Fix bugs

--- Linearlist/26.py
+++ Linearlist/26.py
@@ -2,10 +2,16 @@ class Solution(object):
     def removeDuplicates(self, nums):
         if len(nums):
             for i in range(0,len(nums)):
-                if i<len(nums)-1 and nums[i]==nums[i+1]:
-                    for j in range(i+1,len(nums)):
-                        nums[j-1]=nums[j]
+                if i>=len(nums)-1:
+                    break
+                while nums[i]==nums[i+1]:
+                    for j in range(i,len(nums)-1):
+                        nums[j]=nums[j+1]
                     del nums[-1]
-            del nums[-1]
+                    if i>=len(nums)-1:
+                        break  
         return len(nums)
-        
+    
+a=[1,1,1,1,2,2,2,2,3,3,3,3]
+obj = Solution()
+print obj.removeDuplicates(a)
