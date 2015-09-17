78bc40d07dd666f7a5cf836bc9b9c896dc09e928
Draco <draco.mystack@gmail.com>
2015-09-17 23:02:01 +0800

    Clear the wrong code

--- Linearlist/26.py
+++ Linearlist/26.py
@@ -1,17 +1,13 @@
-# This solution exceed the time limit, cannot be accepted!
 class Solution(object):
     def removeDuplicates(self, nums):
-        if len(nums):
-            for i in range(0,len(nums)):
-                if i>=len(nums)-1:
-                    break
-                while nums[i]==nums[i+1]:
-                    for j in range(i,len(nums)-1):
-                        nums[j]=nums[j+1]
-                    del nums[-1]
-                    if i>=len(nums)-1:
-                        break  
-        return len(nums)
+        if len(nums)==0:
+            return 0
+        index = 0
+        for i in range(1,len(nums)):
+            if nums[index] != nums[i]:
+                index = index+1
+                nums[index] = nums[i]            
+        return index+1
     
 a=[1,1,1,1,2,2,2,2,3,3,3,3]
 obj = Solution()
