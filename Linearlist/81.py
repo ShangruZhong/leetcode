3ae500a1656026472adad1827b1f4cb2b736d4d9
Draco <draco.mystack@gmail.com>
2015-09-20 20:39:47 +0800

    “Search in Rotated Sorted Array II”
    solution accepted

---
+++ Linearlist/81.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def search(self, nums, target):
+		left = 0
+		right = len(nums)
+		while left!=right: 
+			mid = left+(right-left)/2
+			if target == nums[mid]:
+				return True
+			if nums[left]<nums[mid]: # nums[mid] in left sorted array
+				if nums[left]<=target and target<nums[mid]:
+					right = mid
+				else:
+					left = mid+1
+			elif nums[left]>nums[mid]: #nums[mid] in right sorted array, and also the bounder
+				if nums[mid]<target and target<=nums[right-1]:
+					left = mid+1
+				else:
+					right = mid
+			else: #nums[left]=nums[mid]
+				left += 1
+		return False
+
+a = [1,1,3,1,1,1,2]
+obj = Solution()
+print obj.search(a,2)
+print obj.search(a,4)
