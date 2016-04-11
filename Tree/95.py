1eae04ad380fc37d051b57e44715677ebb2bb94c
Draco <draco.mystack@gmail.com>
2016-04-12 00:05:46 +0800

    "Unique Binary Search Trees II"

--- Tree/95.py
+++ Tree/95.py
@@ -1,21 +1,38 @@
 """
-	96. Unique Binary Search Trees
-
-	Dynamic Planning Problem
-	f[i] = sum[f(k-1)*f(i-k)], k = 1 to i
-	return f[n]
-	@date: 2016/04/11
+	95. Unique Binary Search Trees II
+	
+	@date: 2016/04/09
 """
-class Solution(object):
-    def numTrees(self, n):
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def generateTrees(self, n):
         """
         :type n: int
-        :rtype: int
+        :rtype: List[TreeNode]
         """
-        # f = range(0, n+1)
-        f = [0]*(n+1)
-        f[0], f[1] = 1, 1
-        for i in xrange(2, n+1):
-        	for k in xrange(1, i+1):
-        		f[i] += f[k-1]*f[i-k]
-        return f[n]
+        if n == 0:
+        	return []
+        return self.generate(1, n)
+
+    def generate(self, begin, end):
+    	subtree = []
+    	if begin > end:
+    		subtree.append(None)
+    		return subtree
+    	for k in xrange(begin, end + 1):
+    		leftsub = self.generate(begin, k-1)
+    		rightsub = self.generate(k + 1, end)
+    		for i in leftsub:
+    			for j in rightsub:
+    				node = TreeNode(k)
+    				node.left = i
+    				node.right = j
+    				subtree.append(node) 
+    	return subtree
+    
