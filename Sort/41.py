5f0104d30d77829f52f3e939e0c0e4ee0b6a7f1c
Draco <draco.mystack@gmail.com>
2016-05-27 19:13:29 +0800

    Update solution

--- Sort/41.py
+++ Sort/41.py
@@ -2,24 +2,25 @@
 	41. First Missing Positive
 
 	Bucket Sort
+	@date: 2016/05/27
 """
 class Solution(object):
-    def firstMissingPositive(self, nums):
-        """
+	def firstMissingPositive(self, nums):
+		"""
         :type nums: List[int]
         :rtype: int
         """
-        self.bucketSort(nums)
-        for i in xrange(len(nums)):
-            if nums[i] != (i + 1):
-                return (i + 1)
-        return (len(nums) + 1)
-    
-    def bucketSort(self, nums):
-        for i in xrange(len(nums)):
-        	print i, nums[i]
-        	while nums[i] != i + 1:
-        		if nums[i] <= 0 or nums[i] > len(nums) or nums[i] == nums[nums[i]-1]:
-        			break
-            	nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
-    
+		self.bucketSort(nums)
+		for i in xrange(len(nums)):
+			if nums[i] != (i + 1):
+				return i + 1
+		return len(nums) + 1
+
+	def bucketSort(self, nums):
+		for i in xrange(len(nums)):
+			while nums[i] != i + 1:
+				if nums[i] <= 0 or nums[i] > len(nums) or nums[i] == nums[nums[i]-1]:
+					break
+				nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+				# Notice: nums[i] change will influence nums[nums[i]-1]
+
