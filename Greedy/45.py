04ad46e77720253dc5415ad3c18453da6a67611a
Draco <draco.mystack@gmail.com>
2016-05-31 11:24:45 +0800

    "Jump Game II"

---
+++ Greedy/45.py
@@ -0,0 +1,20 @@
+"""
+	45. Jump Game II
+
+	@date: 2016/05.31
+"""
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        minStep = 0
+        maxStep = 0
+        cur = 0
+        for i in xrange(len(nums)):
+            if i > maxStep:
+                maxStep = cur
+                minStep += 1
+            cur = max(cur, i + nums[i])
+        return minStep
