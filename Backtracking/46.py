da0b92414903078dda9734323977ade3762f6c9d
Code Monkey <draco.mystack@gmail.com>
2016-10-07 23:02:29 +0800

    “Permutations”

---
+++ Backtracking/46.py
@@ -0,0 +1,23 @@
+"""
+	46. Permutations
+
+	Backtracking
+	@date: 2016/10/07
+"""
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.backtracking(nums, [], res)
+        return res
+        
+    def backtracking(self, nums, path, res):
+        if not nums:
+            res.append(path)
+        for i in xrange(len(nums)):
+            # foreach sub-node of nums[i], the rest exclude nums[i]
+            rest = nums[:i] + nums[i + 1:]
+            self.backtracking(rest, path + [nums[i]], res)
