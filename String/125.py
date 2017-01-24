bd7ef532f123eacdeadb38a170f651aee9dc621c
Shangru <draco.mystack@gmail.com>
2017-01-24 13:47:09 +0800

    “Valid Palindrome” updated

--- String/125.py
+++ String/125.py
@@ -36,4 +36,21 @@ class Solution(object):
                 left += 1
         return True
 
+    # solution 2
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l <r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l +=1; r -= 1
+        return True
+
 
