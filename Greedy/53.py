d88a815697aeb38e46e15f2796d1fb4e1b6ad62e
Shangru <draco.mystack@gmail.com>
2017-02-07 20:46:25 +0800

    “Maximum Subarray”

---
+++ Greedy/53.py
@@ -0,0 +1,21 @@
+"""
+    53. Maximum Subarray
+
+    Classical DP problem, but use greedy method
+    sum[i] = max(sum[i - 1] + nums[i], nums[i])
+    @date: 2016/11/20
+"""
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not len(nums):
+            return 0
+        s = nums[0]
+        smax = nums[0]
+        for i in xrange(1, len(nums)):
+            s = max(s + nums[i], nums[i])
+            smax = max(s, max)
+        return smax
