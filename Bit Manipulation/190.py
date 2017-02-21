5ad54590136b5a7095dff80f94099f74c9195ad3
Shangru <draco.mystack@gmail.com>
2017-02-21 20:26:11 +0800

    “Reverse Bits”

---
+++ Bit Manipulation/190.py
@@ -0,0 +1,17 @@
+"""
+	190. Reverse Bits
+
+	Inspired by merge sort:
+	1234 5678->5678 1234->78 56 34 12->87654321
+	@date: 2017/02/21
+"""
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n
